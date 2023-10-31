1f3628696edbfef0d8d4ec0b8b499a021a3cc183
aryanbahnan08 <146058181+aryanbahnan08@users.noreply.github.com>
2023-10-31 13:34:58 +0800

    kuliah algo tgl 31

---
+++ 01-programjavapertama/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       //program biodata mahsiswa 
+        String nama;
+        string nim;
+        String tgl lahir;
+        String alamat;
+        float beratbadan;
+        float tingibandan;
+
+//mengisi fareabel  
+nama =aryan;
+nim=23241039
+tgl-lahir=8 april 2004
+alamat=bima dompu;
+berat-badan=80 of;
+tinggi-badan=180 of;
+
+//cetak biodata
+System.out.println("============================");
+System.err.println(" Nama : " + nama);
+System.err.println("Nim : " + nim);
+System.out.println("Tanggal_lahir : " + Tgl_lahir);
+System.out.println("Berat_Badan : " + berat_badam);
+System.out.println("Tinggi_Badan : " + tinggi_badan);
+System.out.println("=============================");
