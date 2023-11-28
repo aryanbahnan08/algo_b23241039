734e8cdb0d4ae845a2ddea42415d0264f1c47eef
aryanbahnan08 <146058181+aryanbahnan08@users.noreply.github.com>
2023-11-28 12:54:34 +0800

    Add files via upload

---
+++ 03-TIPE DATA.java
@@ -0,0 +1,80 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+// tipe data di java
+// integer,byte, short, long, double, float,
+char,boolean
+
+//integer (satuan)
+int i =10;
+System.out('======INTEGRER=====");
+System.out.println("nilai integer i = "  + i);
+Sytem.out.println ("NILAI MAX; " + integer.           
+MAX_VALUE);
+System.out.println("besar integer :  "  + integer  .SIZE + "bit")P;
+
+// byte
+byte b = 10;
+System.out('=====BYTE=====");
+System.out.println("nilai BYTE B = "  + B);
+Sytem.out.println ("NILAI MAX; " + BYTE.           
+MAX_VALUE);
+System.out.println("besar BYTE :  "  + BYTE .SIZE + "bit")P;
+
+//SHORT
+SHORT S=10;
+System.out('=====SHORT======");
+System.out.println("nilai SHORT S  = "  + S);
+Sytem.out.println ("NILAI MAX; " + .           
+MAX_VALUE);
+System.out.println("besar  : SHORT  "  + SHORT  .SIZE + "bit")P;
+
+//LONG
+LONG L=10;
+System.out('=====LONG======");
+System.out.println("nilai LONG L  = "  + L);
+Sytem.out.println ("NILAI MAX; " + .           
+MAX_VALUE);
+System.out.println("besar  : LONG "  + LONG  .SIZE + "bit")P;
+
+//DOUBLE
+DOUBLE D=10
+System.out('=====DOUBLE======");
+System.out.println("nilai DOUBLE D  = "  + D);
+Sytem.out.println ("NILAI MAX; " + .           
+MAX_VALUE);
+System.out.println("besar  : DOUBLE "  + DOUBLE  .SIZE + "bit")P;
+
+//FLOAT
+FLOAT F=10;
+System.out('=====FFLOAT======");
+System.out.println("nilai FLOAT F  = "  + F );
+Sytem.out.println ("NILAI MAX; " +.           
+MAX_VALUE);
+System.out.println("besar  : FLOAT "  + FLOAT .SIZE + "bit")P;
+
+//CHAR
+CHAR C=10;
+System.out('======CHAR=====");
+System.out.println("nila iCHAR C = "  + C);
+Sytem.out.println ("NILAI MAX; " +CHAR.           
+MAX_VALUE);
+System.out.println("besar CHAR :  "  + CHAR  .SIZE + "bit")P;
+
+//BOOLEAN
+BOOLEAN B=10;
+System.out('=====BOOLEAN======");
+System.out.println("nilai BOOLEAN B  = "  + B );
+Sytem.out.println ("NILAI MAX; " + .           
+MAX_VALUE);
+System.out.println("besar  : BOOLEAN "  + BOOLEAN  .SIZE + "bit")P;
+
+
+
+                         
+
+
+
+
+
